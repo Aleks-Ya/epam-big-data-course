36390e8f60456c8a0102f0736830347a7522451e
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-03 14:24:16 +0300

    verify output file content

--- Spark/lesson1/src/test/scala/lesson1/RealDataTest.scala
+++ Spark/lesson1/src/test/scala/lesson1/RealDataTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.Matchers._
 import scala.io._
 import java.io.File
 import org.scalatest.BeforeAndAfterAll
-
+import java.nio.file.Files
 class RealDataTest extends FlatSpec with BeforeAndAfterAll {
 
   var sc: SparkContext = null
@@ -29,14 +29,20 @@ class RealDataTest extends FlatSpec with BeforeAndAfterAll {
     val al = new AccessLogTask()
     al.processFile(sc, inputFile, outputFile)
     val top5 = al.getTop5()
-    println("Top5: " + top5)
+    println("Top5:\n" + top5)
 
+    val browsers = al.getBrowsers()
     val browsersExp = "IE: 0\nMozilla: 12280\nOthers: 1221\n"
-    al.getBrowsers() shouldEqual browsersExp
+    println("Browsers:\n" + browsers)
+    browsers shouldEqual browsersExp
 
     val size = outputFile.length()
-    println("Output file size: " + size)
+    val lines = Files.lines(new File(outputFile, "part-00000").toPath()).toArray
     assert(size == 4096L)
+    assert("ip32,57503,16273379".equals(lines(0)))
+    assert("ip1165,66003,12474621".equals(lines(1)))
+    assert("ip16,95122,12080603".equals(lines(2)))
+    assert("ip75,0,0".equals(lines.last))
   }
 
   override def afterAll() {
