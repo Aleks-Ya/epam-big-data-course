af64aba10f5ce5ec24fdac884bf879370dc7943b
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-07 22:16:19 +0300

    save event log for Spark History Server

--- Spark/lesson2/src/test/scala/lesson2/AirportReportTest.scala
+++ Spark/lesson2/src/test/scala/lesson2/AirportReportTest.scala
@@ -1,16 +1,20 @@
 package lesson2
+
 import lesson2.loader.CsvResourceLoader
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
-import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.Matchers._
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 
 class AirportReportTest extends FlatSpec with BeforeAndAfterAll {
   var sc: SparkContext = _
   var sql: SQLContext = _
 
   override def beforeAll {
-    val conf = new SparkConf().setAppName("SmallDataTest").setMaster("local")
+    val conf = new SparkConf()
+      .setAppName("SmallDataTest")
+      .setMaster("local")
+    setEventLogDir(conf)
     sc = new SparkContext(conf)
     sql = new SQLContext(sc)
   }
@@ -21,9 +25,19 @@ class AirportReportTest extends FlatSpec with BeforeAndAfterAll {
     val result = processor.calculate
 
     result.biggestCarrier shouldEqual "Southwest Airlines Co."
-    result.busiestAirportsUsaJuneAug should contain inOrderOnly ("Perry-Warsaw (01G)", "Livingston Municipal (00R)", "LaGuardia (LGA)")
+    result.busiestAirportsUsaJuneAug should contain inOrderOnly("Perry-Warsaw (01G)", "Livingston Municipal (00R)", "LaGuardia (LGA)")
     result.flightsNycJune2007 shouldEqual 1
     result.flightsPerCarrierIn2007 shouldEqual 3
+  }
+
+  private def setEventLogDir(conf: SparkConf) = {
+    val logDir = sys.env.get("SPARK_HISTORY_FS_LOG_DIRECTORY")
+    if (logDir.isDefined) {
+      conf
+        .set("spark.eventLog.enabled", "true")
+        .set("spark.eventLog.dir", logDir.get)
+      println("Set event log dir: " + logDir.get)
+    }
   }
 
   override def afterAll {
