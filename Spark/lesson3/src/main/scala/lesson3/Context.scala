a93b641f28c72374658b30cedde2d243bd75bf18
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-13 17:03:50 +0300

    Lesson 3: use NetworkInterfaceReceiver

--- Spark/lesson3/src/main/scala/lesson3/Context.scala
+++ Spark/lesson3/src/main/scala/lesson3/Context.scala
@@ -5,12 +5,12 @@ import java.nio.file.Files
 import lesson3.event.service.{ConsoleEventService, EventService}
 import lesson3.hive.{HardcodedHiveService, HiveService}
 import lesson3.kafka.{KafkaService, KafkaServiceImpl}
-import lesson3.net.{FakeReceiver, TcpPacket}
+import lesson3.net.{NetworkInterfaceReceiver, TcpPacket}
 import lesson3.settings.service.{HardcodedSettingsService, SettingsService}
 import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.streaming.receiver.Receiver
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.receiver.Receiver
 import org.slf4j.LoggerFactory
 
 object Context {
@@ -37,7 +37,7 @@ object Context {
     }
   }
 
-  val receiver: Receiver[TcpPacket] = new FakeReceiver(2000)
+  val receiver: Receiver[TcpPacket] = new NetworkInterfaceReceiver
 
   val kafkaService: KafkaService = KafkaServiceImpl
   val eventService: EventService = new ConsoleEventService
--- Spark/lesson3/src/main/scala/lesson3/net/NetworkInterfaceReceiver.scala
+++ Spark/lesson3/src/main/scala/lesson3/net/NetworkInterfaceReceiver.scala
@@ -5,15 +5,18 @@ import java.util.concurrent.{Callable, ExecutorService, Executors}
 import org.apache.spark.api.java.StorageLevels
 import org.apache.spark.streaming.receiver.Receiver
 import org.pcap4j.core.{BpfProgram, PcapHandle, Pcaps}
-import org.pcap4j.packet.{EthernetPacket, Packet}
+import org.pcap4j.packet.{IpV4Packet, Packet}
+import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
 class NetworkInterfaceReceiver extends Receiver[TcpPacket](StorageLevels.MEMORY_ONLY) {
+  private val log = LoggerFactory.getLogger(getClass)
   private val tcpFilter = "tcp"
   private var pool: ExecutorService = _
 
   override def onStart() {
+    log.debug("Starting " + getClass.getSimpleName)
     val callables = Pcaps.findAllDevs.asScala
       .map(nif => new PcapHandle.Builder(nif.getName).build())
       .map(handle => {
@@ -24,11 +27,18 @@ class NetworkInterfaceReceiver extends Receiver[TcpPacket](StorageLevels.MEMORY_
         new Callable[Unit] {
           override def call(): Unit = {
             while (!Thread.currentThread().isInterrupted) {
-              val packet: Packet = handle.getNextPacket
-              if (packet != null) {
-                val payload = packet.getPayload
-                store(convertToTcpPacket(packet))
-                println(payload)
+              try {
+                while (!Thread.currentThread().isInterrupted) {
+                  val packet: Packet = handle.getNextPacket
+                  if (packet != null) {
+                    val payload = packet.getPayload
+                    if (payload.isInstanceOf[IpV4Packet]) {
+                      store(convertToTcpPacket(payload))
+                    }
+                  }
+                }
+              } catch {
+                case e: Exception => log.error("Exception, restart", e)
               }
             }
           }
@@ -37,13 +47,17 @@ class NetworkInterfaceReceiver extends Receiver[TcpPacket](StorageLevels.MEMORY_
 
     pool = Executors.newFixedThreadPool(callables.length)
     pool.invokeAll(callables.asJava)
+
+    log.info(getClass.getSimpleName + " started")
   }
 
   private def convertToTcpPacket(packet: Packet) = {
-    val ep = packet.asInstanceOf[EthernetPacket]
-    val srcAddr = ep.getHeader.getSrcAddr.toString
+    val ep = packet.asInstanceOf[IpV4Packet]
+    val srcAddr = ep.getHeader.getSrcAddr.getHostAddress
     val length = ep.length
-    new TcpPacket(srcAddr, length)
+    val tcpPacket = new TcpPacket(srcAddr, length)
+    log.debug("Created: " + tcpPacket)
+    tcpPacket
   }
 
   override def onStop(): Unit = {
