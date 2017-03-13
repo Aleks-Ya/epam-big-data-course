93797c3bc3f12548bb50b672d1c950c874b8b987
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-13 11:10:21 +0300

    Lesson 3: init SparkContext in Context

--- Spark/lesson3/src/main/scala/lesson3/Context.scala
+++ Spark/lesson3/src/main/scala/lesson3/Context.scala
@@ -1,10 +1,43 @@
 package lesson3
 
+import java.nio.file.Files
+
 import lesson3.event.service.{ConsoleEventService, EventService}
 import lesson3.hive.{HardcodedHiveService, HiveService}
 import lesson3.kafka.{KafkaService, KafkaServiceImpl}
+import lesson3.net.{FakeReceiver, TcpPacket}
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.receiver.Receiver
+import org.slf4j.LoggerFactory
 
 object Context {
+  private val log = LoggerFactory.getLogger(getClass)
+
+  val sparkContext: SparkContext = {
+    val conf = new SparkConf().setAppName("YablokovSpark3").setMaster("local[2]")
+    new SparkContext(conf)
+  }
+
+  val streamingContext: StreamingContext = {
+    val batchDuration = Seconds(5)
+    val ssc = new StreamingContext(sparkContext, batchDuration)
+    ssc.checkpoint(Files.createTempDirectory("checkpoint_").toString)
+    ssc
+  }
+
+  val hiveContext: HiveContext = {
+    try {
+      new HiveContext(sparkContext)
+    } catch {
+      case e: Exception => log.error("Hive context isn't initialized", e)
+        null
+    }
+  }
+
+  val receiver: Receiver[TcpPacket] = new FakeReceiver(2000)
+
   val kafkaService: KafkaService = KafkaServiceImpl
   val eventService: EventService = new ConsoleEventService
   val hiveService: HiveService = HardcodedHiveService
--- Spark/lesson3/src/main/scala/lesson3/Main.scala
+++ Spark/lesson3/src/main/scala/lesson3/Main.scala
@@ -1,50 +1,23 @@
 package lesson3
 
-import java.nio.file.Files
-
-import lesson3.kafka.{ConsoleKafkaService, KafkaService, KafkaServiceImpl}
-import lesson3.net.{FakeReceiver, NetworkInterfaceReceiver}
-import lesson3.settings.service.HardcodedSettingsService
+import lesson3.kafka.KafkaService
 import lesson3.spark.TrafficAnalyzer
-import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.StreamingContext
 import org.slf4j.LoggerFactory
 
 object Main {
   private val log = LoggerFactory.getLogger(getClass)
 
   def main(args: Array[String]) {
-    var sc: SparkContext = null
-    var ssc: StreamingContext = null
+    val ssc: StreamingContext = Context.streamingContext
     val kafkaService: KafkaService = Context.kafkaService
     try {
-      //      val hdfsDir = if (args.isEmpty) "hdfs://sandbox.hortonworks.com:8020/tmp/iablokov/spark/lesson2" else args(0)
-      val conf = new SparkConf().setAppName("YablokovSpark3").setMaster("local[2]")
-      //      sc = new SparkContext(conf)
-      //      val hive = new HiveContext(sc)
-      //      hive.sql("CREATE TABLE IN NOT EXISTS settings (ip STRING, type INT, value DOUBLE, period BIGINT);")
-      //      val nullSettingsExists = hive.sql("SELECT ip FROM settings WHERE ip = 'NULL'").count() > 0
-      //      if (!nullSettingsExists) {
-      //        hive.sql("INSERT INTO settings(ip, type, value, period) VALUES ('NULL', 1, 0, 0);")
-      //        hive.sql("INSERT INTO settings(ip, type, value, period) VALUES ('NULL', 2, 0, 0);")
-      //      }
-
-      val batchDuration = Seconds(5)
-      val ssc = new StreamingContext(conf, batchDuration)
-      sc = ssc.sparkContext
-      ssc.checkpoint(Files.createTempDirectory("checkpoint_").toString)
-      val stream = ssc.receiverStream(new FakeReceiver(2000))
-      kafkaService.start()
+      val receiver = Context.receiver
+      val stream = ssc.receiverStream(receiver)
       new TrafficAnalyzer(stream)
-
+      kafkaService.start()
       ssc.start()
       ssc.awaitTermination()
-
-      //      val sql = new SQLContext(sc)
-      //      val hive = new HiveContext(sc)
-
-
     } finally {
       try {
         if (ssc != null) {
