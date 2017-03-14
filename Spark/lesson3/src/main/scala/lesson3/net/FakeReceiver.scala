3aac7ab2ad138a2af6494c2033058fd8f29d2535
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-14 22:04:44 +0300

    Lesson 3: fix init FakeReceiver

--- Spark/lesson3/src/main/scala/lesson3/net/FakeReceiver.scala
+++ Spark/lesson3/src/main/scala/lesson3/net/FakeReceiver.scala
@@ -3,9 +3,10 @@ package lesson3.net
 import org.apache.spark.api.java.StorageLevels
 import org.apache.spark.streaming.receiver.Receiver
 
-class FakeReceiver(private val intervalMillis: Long = 2000L)
+class FakeReceiver
   extends Receiver[TcpPacket](StorageLevels.MEMORY_ONLY) {
 
+  private val intervalMillis: Long = 2000L
   private var thread: Thread = _
   private val packet1 = new TcpPacket("100.200.300.400", 1000)
   private val packet2 = new TcpPacket("200.300.400.500", 5000)
