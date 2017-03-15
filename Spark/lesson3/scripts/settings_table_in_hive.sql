73ef8e702eee3254768cebc04cff526f851bd0ee
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-15 10:11:48 +0300

    Lesson 3: fix settings init script

--- Spark/lesson3/scripts/settings_table_in_hive.sql
+++ Spark/lesson3/scripts/settings_table_in_hive.sql
@@ -7,11 +7,11 @@ CREATE TABLE settings (
 
 SELECT * FROM settings;
 
-INSERT INTO settings(ip, type, value, period) VALUES ("NULL", 1, 1, 10);
-INSERT INTO settings(ip, type, value, period) VALUES ("NULL", 0, 2, 20);
+INSERT INTO settings(ip, type, value, period) VALUES ("NULL", 2, 1, 10); --default
+INSERT INTO settings(ip, type, value, period) VALUES ("NULL", 1, 2, 20); --default
 
-INSERT INTO settings(ip, type, value, period) VALUES ("173.194.73.101", 0, 3, 20); --google.com
-INSERT INTO settings(ip, type, value, period) VALUES ("173.194.73.101", 1, 2, 10); --google.com
+INSERT INTO settings(ip, type, value, period) VALUES ("173.194.73.101", 1, 3, 20); --google.com
+INSERT INTO settings(ip, type, value, period) VALUES ("173.194.73.101", 2, 2, 10); --google.com
 
-INSERT INTO settings(ip, type, value, period) VALUES ("10.16.0.4", 0, 4, 20); --epam.com
-INSERT INTO settings(ip, type, value, period) VALUES ("10.16.0.4", 1, 3, 10); --epam.com
+INSERT INTO settings(ip, type, value, period) VALUES ("10.16.0.4", 1, 4, 20); --epam.com
+INSERT INTO settings(ip, type, value, period) VALUES ("10.16.0.4", 2, 3, 10); --epam.com
