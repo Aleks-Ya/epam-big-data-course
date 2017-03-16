b280403150764b28d62eeaa0c72c2b6d361911cd
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-16 04:01:38 +0300

    Lesson 3: fix warnings in TrafficAnalyzerSteps

--- Spark/lesson3/src/main/scala/lesson3/spark/TrafficAnalyzerSteps.scala
+++ Spark/lesson3/src/main/scala/lesson3/spark/TrafficAnalyzerSteps.scala
@@ -1,16 +1,17 @@
 package lesson3.spark
 
 import lesson3.net.TcpPacket
+import lesson3.settings.IpSettings
 
 object TrafficAnalyzerSteps {
-  val mapIpToPacket = (packet: TcpPacket) => (packet.ip, packet)
+  val mapIpToPacket: TcpPacket => ((String, TcpPacket)) = packet => (packet.ip, packet)
 
-  val reducePacketSize = (p1: TcpPacket, p2: TcpPacket) => {
+  val reducePacketSize: (TcpPacket, TcpPacket) => TcpPacket = (p1, p2) => {
     assert(p1.ip == p2.ip)
     new TcpPacket(p1.ip, p1.size + p2.size)
   }
 
-  val addSettings = (pair: (String, TcpPacket)) => {
+  val addSettings: ((String, TcpPacket)) => (String, (TcpPacket, IpSettings)) = pair => {
     val ip = pair._1
     val packet = pair._2
     val settings = TrafficAnalyzerHelper.settingsByIp(ip)
