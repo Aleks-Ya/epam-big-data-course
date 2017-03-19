9e9c05e711bd45b8ae4a578c501e309a995cc816
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-20 02:33:00 +0300

    Lesson 4: evaluation

--- Spark/lesson4/src/main/scala/lesson4/MainDataFrame.scala
+++ Spark/lesson4/src/main/scala/lesson4/MainDataFrame.scala
@@ -1,5 +1,6 @@
 package lesson4
 
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.linalg.{VectorUDTPublic, Vectors}
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.{Row, SparkSession}
@@ -12,7 +13,7 @@ object MainDataFrame {
 
   def main(args: Array[String]): Unit = {
     val ss = SparkSession.builder()
-      .appName("Iablokov Lesson 3")
+      .appName("Iablokov Lesson 4")
       .master("local[*]")
       .getOrCreate()
 
@@ -38,11 +39,12 @@ object MainDataFrame {
       .withColumn("id", monotonically_increasing_id())
     labels.show
 
-    val labelledVectors = vectorsDf.join(labels, "id")
-    labelledVectors.show
+    val labelledVectors = vectorsDf.join(labels, "id").randomSplit(Array[Double](1, 1))
+    val trainingData = labelledVectors(0)
+    val testData = labelledVectors(1)
 
     val estimator = new LinearRegression().setMaxIter(10).setLabelCol("label").setFeaturesCol("features")
-    val model = estimator.fit(labelledVectors)
+    val model = estimator.fit(trainingData)
 
     val trainingSummary = model.summary
     println(s"numIterations: ${trainingSummary.totalIterations}")
@@ -50,6 +52,15 @@ object MainDataFrame {
     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
     println(s"r2: ${trainingSummary.r2}")
     trainingSummary.residuals.show()
+
+    val predictions = model.transform(testData)
+    predictions.show
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("rmse")
+    val rmse = evaluator.evaluate(predictions)
+    println(rmse)
 
     ss.close
   }
