e0df0fb0f9744e8b8f18f5e627a0f486dccde743
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-26 22:44:31 +0300

    Create Main

--- Hadoop/HW_module_1/src/main/scala/module1/hw1/Main.scala
+++ Hadoop/HW_module_1/src/main/scala/module1/hw1/Main.scala
@@ -14,7 +14,18 @@ object Main {
     val config = new Configuration()
     val url = new URI("hdfs://sandbox.hortonworks.com:8020")
     val fs = FileSystem.get(url, config)
-    log.info("Status: " + fs.getStatus(new Path("/")))
+    log.info("FileSystem status: " + fs.getStatus)
+    val files = args(0)
+    val statuses = fs.globStatus(new Path(files))
+    val counters = statuses
+      .map(status => status.getPath)
+      .map(path => fs.open(path))
+      .toList
+
+    val top100Map = Processor.process(counters, 100)
+    println(top100Map)
+
+    log.info("Finish")
   }
 }
 
---
+++ Hadoop/HW_module_1/src/main/scala/module1/hw1/MainLocal.scala
@@ -0,0 +1,19 @@
+package module1.hw1
+
+import java.io.{File, FileInputStream}
+
+import org.slf4j.LoggerFactory
+
+object MainLocal {
+  private val log = LoggerFactory.getLogger(getClass)
+
+  def main(args: Array[String]): Unit = {
+    log.info("Start")
+    //    val files = new File("""c:\tmp\ipinyou\""").listFiles().map(file => new FileInputStream(file)).toList
+    val files = List(new FileInputStream("""c:\tmp\ipinyou\bid.20130612.txt"""))
+    val top100Map = Processor.process(files, 100)
+    println(top100Map)
+    log.info("Finish")
+  }
+}
+
