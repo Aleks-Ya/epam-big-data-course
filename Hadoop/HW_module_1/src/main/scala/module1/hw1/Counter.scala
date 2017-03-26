b13021319ebcb2ca4531b4e78c110be9a665aaae
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-26 22:43:45 +0300

    Add logging

--- Hadoop/HW_module_1/src/main/scala/module1/hw1/Counter.scala
+++ Hadoop/HW_module_1/src/main/scala/module1/hw1/Counter.scala
@@ -3,7 +3,11 @@ package module1.hw1
 import java.io.{BufferedReader, InputStream, InputStreamReader}
 import java.util.concurrent.Callable
 
+import org.slf4j.LoggerFactory
+
 class Counter(private val is: InputStream) extends Callable[Map[String, Int]] {
+  private val log = LoggerFactory.getLogger(getClass)
+  var lineProcessed = 0L
 
   override def call(): Map[String, Int] = {
     val reader = new BufferedReader(new InputStreamReader(is))
@@ -16,12 +20,16 @@ class Counter(private val is: InputStream) extends Callable[Map[String, Int]] {
       val id = Helper.parseIPinYouID(line)
       if (idCountMap.contains(id)) {
         val count = idCountMap(id) + 1
+        if (count % 20 == 0) log.debug(s"$id->$count")
         idCountMap += id -> count
       } else {
         idCountMap += id -> 1
       }
+      lineProcessed += 1
     }
-    idCountMap.toMap
+    val immutableMap = idCountMap.toMap
+    log.info(s"Counter finished. Processed $lineProcessed lines. Map size ${immutableMap.size}.")
+    immutableMap
   }
 
 }
--- Hadoop/HW_module_1/src/main/scala/module1/hw1/Processor.scala
+++ Hadoop/HW_module_1/src/main/scala/module1/hw1/Processor.scala
@@ -3,18 +3,30 @@ package module1.hw1
 import java.io.InputStream
 import java.util.concurrent.Executors
 
+import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConverters._
 
+
 object Processor {
+  private val log = LoggerFactory.getLogger(getClass)
+
   def process(streams: List[InputStream], topElements: Int): Map[String, Int] = {
     val counters = streams.map(is => new Counter(is))
+    log.info("Threads count: " + counters.size)
     val pool = Executors.newFixedThreadPool(counters.size)
+    log.info("Invoke counters")
     val futures = pool.invokeAll(counters.asJava).asScala
-    pool.shutdown()
+    log.info("Counters invoked")
     val idCountMaps = futures.map(future => future.get).toList
+    log.info("Counters finished" + idCountMaps.size)
+    pool.shutdown()
     val joinedMap = Helper.joinMaps(idCountMaps)
+    log.info("Join finished Map size=" + idCountMaps.size)
     val sortedMap = Helper.sortMap(joinedMap)
+    log.info("Map sorted")
     val top100Map = sortedMap.take(topElements)
+    log.info("top 100 found")
     top100Map
   }
 }
