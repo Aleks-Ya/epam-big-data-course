8509e86217d744e7ae9f67441a8519823080940f
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-03-27 19:11:55 +0300

    fix compile SortedFixedSizeListTest

--- Hadoop/HW_module_1/src/test/scala/module1/hw1/SortedFixedSizeListTest.scala
+++ Hadoop/HW_module_1/src/test/scala/module1/hw1/SortedFixedSizeListTest.scala
@@ -7,11 +7,11 @@ class SortedFixedSizeListTest extends FlatSpec with Matchers {
   it should "parse a data file's line" in {
     val list = new SortedFixedSizeList(3)
 
-    val a = ("a", 1)
-    val b = ("b", 2)
-    val c = ("c", 3)
-    val d = ("d", 3)
-    val e = ("e", 4)
+    val a = (10L, 1)
+    val b = (11L, 2)
+    val c = (12L, 3)
+    val d = (13L, 3)
+    val e = (14L, 4)
 
     list.add(d)
     list.add(e)
