bdee93c477822f2efda1b9fb0096d691f772e0de
Aleksey Yablokov <ya_al@bk.ru>
2017-03-29 22:35:13 +0300

    refactor Main

--- Hadoop/HW_module_2/src/main/java/module1/hw2/Main.java
+++ Hadoop/HW_module_2/src/main/java/module1/hw2/Main.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
@@ -24,34 +25,17 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         log.info("Start");
-
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://localhost:9000");
-
-        Job job = Job.getInstance(conf, "The longest word");
-        job.setJarByClass(Main.class);
-        ChainMapper.addMapper(job, StringToWordMapper.class, NullWritable.class, Text.class, Text.class, NullWritable.class, conf);
-        ChainMapper.addMapper(job, LongestWordCombiner.class, Text.class, NullWritable.class, Text.class, NullWritable.class, conf);
-        job.setReducerClass(LongestWordReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        Path inputFile = new Path(args[0]);
-        log.info("Input file: " + inputFile);
-        FileInputFormat.addInputPath(job, inputFile);
-
+        Configuration conf = makeConfig();
+        Job job = makeJob(conf);
+        configureInputFile(job, args[0]);
         FileSystem fs = FileSystem.get(conf);
-        Path outputDir = new Path(args[1]);
-        log.info("Output dir: " + outputDir);
-        if (fs.exists(outputDir)) {
-            log.info("Output dir exists. Delete it: " + fs.delete(outputDir, true));
-        }
-        FileOutputFormat.setOutputPath(job, outputDir);
+        Path outputDir = configureOutputDir(job, args[1], fs);
+        executeJob(job);
+        printResultToConsole(fs, outputDir);
+        log.info("Exit");
+    }
 
-        log.info("Run job");
-        boolean success = job.waitForCompletion(true);
-        log.info("Job finished");
-
+    private static void printResultToConsole(FileSystem fs, Path outputDir) throws IOException {
         FileStatus[] outputFiles = fs.listStatus(outputDir);
         StringBuilder longestWord = new StringBuilder();
         for (FileStatus outputFile : outputFiles) {
@@ -62,8 +46,47 @@ public class Main {
 
         }
         log.info("THE LONGEST WORD: " + longestWord);
+    }
 
-        log.info("Exit");
-        System.exit(success ? 0 : 1);
+    private static void executeJob(Job job) throws IOException, InterruptedException, ClassNotFoundException {
+        log.info("Run job");
+        boolean success = job.waitForCompletion(true);
+        log.info("Job finished");
+        if (!success) {
+            throw new RuntimeException("Job filed");
+        }
+    }
+
+    private static Path configureOutputDir(Job job, String arg, FileSystem fs) throws IOException {
+        Path outputDir = new Path(arg);
+        log.info("Output dir: " + outputDir);
+        if (fs.exists(outputDir)) {
+            log.info("Output dir exists. Delete it: " + fs.delete(outputDir, true));
+        }
+        FileOutputFormat.setOutputPath(job, outputDir);
+        return outputDir;
+    }
+
+    private static void configureInputFile(Job job, String arg) throws IOException {
+        Path inputFile = new Path(arg);
+        log.info("Input file: " + inputFile);
+        FileInputFormat.addInputPath(job, inputFile);
+    }
+
+    private static Job makeJob(Configuration conf) throws IOException {
+        Job job = Job.getInstance(conf, "The longest word");
+        job.setJarByClass(Main.class);
+        ChainMapper.addMapper(job, StringToWordMapper.class, NullWritable.class, Text.class, Text.class, NullWritable.class, conf);
+        ChainMapper.addMapper(job, LongestWordCombiner.class, Text.class, NullWritable.class, Text.class, NullWritable.class, conf);
+        job.setReducerClass(LongestWordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        return job;
+    }
+
+    private static Configuration makeConfig() {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://localhost:9000");
+        return conf;
     }
 }
