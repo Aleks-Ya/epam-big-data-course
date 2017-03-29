af6f2ae67039180b8cd91b5756e0b1b7130f1e01
Aleksey Yablokov <ya_al@bk.ru>
2017-03-29 21:57:13 +0300

    print result to console

--- Hadoop/HW_module_2/src/main/java/module1/hw2/Main.java
+++ Hadoop/HW_module_2/src/main/java/module1/hw2/Main.java
@@ -1,6 +1,8 @@
 package module1.hw2;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,6 +14,10 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -35,16 +41,27 @@ public class Main {
         FileInputFormat.addInputPath(job, inputFile);
 
         FileSystem fs = FileSystem.get(conf);
-        Path outputFile = new Path(args[1]);
-        log.info("Output file: " + outputFile);
-        if (fs.exists(outputFile)) {
-            log.info("Output file exists. Delete it: " + fs.delete(outputFile, true));
+        Path outputDir = new Path(args[1]);
+        log.info("Output dir: " + outputDir);
+        if (fs.exists(outputDir)) {
+            log.info("Output dir exists. Delete it: " + fs.delete(outputDir, true));
         }
-        FileOutputFormat.setOutputPath(job, outputFile);
+        FileOutputFormat.setOutputPath(job, outputDir);
 
         log.info("Run job");
         boolean success = job.waitForCompletion(true);
         log.info("Job finished");
+
+        FileStatus[] outputFiles = fs.listStatus(outputDir);
+        StringBuilder longestWord = new StringBuilder();
+        for (FileStatus outputFile : outputFiles) {
+            try (FSDataInputStream resultIS = fs.open(outputFile.getPath())) {
+                String content = new BufferedReader(new InputStreamReader(resultIS)).lines().collect(Collectors.joining());
+                longestWord.append(content);
+            }
+
+        }
+        log.info("THE LONGEST WORD: " + longestWord);
 
         log.info("Exit");
         System.exit(success ? 0 : 1);
--- Hadoop/HW_module_2/src/main/java/module1/hw2/StringToWordMapper.java
+++ Hadoop/HW_module_2/src/main/java/module1/hw2/StringToWordMapper.java
@@ -1,5 +1,6 @@
 package module1.hw2;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -7,12 +8,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class StringToWordMapper extends Mapper<NullWritable, Text, Text, NullWritable> {
+public class StringToWordMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
 
     private Text word = new Text();
 
     @Override
-    public void map(NullWritable key, Text value, Context context) throws IOException, InterruptedException {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         StringTokenizer itr = new StringTokenizer(value.toString());
         while (itr.hasMoreTokens()) {
             word.set(itr.nextToken());
