1850db517c8eb72cfd01597b920a4c127d2fe129
Aleksey Yablokov <ya_al@bk.ru>
2017-03-29 23:25:30 +0300

    use split instead of StringTokenizer

--- Hadoop/HW_module_2/src/main/java/module1/hw2/StringToWordMapper.java
+++ Hadoop/HW_module_2/src/main/java/module1/hw2/StringToWordMapper.java
@@ -6,18 +6,18 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class StringToWordMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
-
-    private Text word = new Text();
+    private final Pattern separator = Pattern.compile("\\s");
+    private final Text text = new Text();
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, NullWritable.get());
+        String[] words = separator.split(value.toString());
+        for (String word : words) {
+            text.set(word);
+            context.write(text, NullWritable.get());
         }
     }
 }
