e6595c283af7c3a666c62bf73ea9435339f7daad
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-10 10:52:46 +0300

    Hive HW3: draft

---
+++ Hive/Hive_HW3/src/main/java/hive/hw3/UserAgentUdf2.java
@@ -0,0 +1,24 @@
+package hive.hw3;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+
+public class UserAgentUdf2 extends GenericUDF {
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+        return PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+        return null;
+    }
+
+    @Override
+    public String getDisplayString(String[] strings) {
+        return "Parse User-Agent header";
+    }
+}
---
+++ Hive/Hive_HW3/src/test/java/hive/hw3/UserAgentUdfTest.java
@@ -0,0 +1,12 @@
+package hive.hw3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserAgentUdfTest {
+    @Test
+    public void evaluate() throws Exception {
+    }
+
+}
