4ecdf8105eda9f5347fbc6cf878ddf407530921e
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-10 14:38:13 +0300

    add test for firefox

--- Hive/Hive_HW3/src/test/java/hive/hw3/UserAgentUdf2Test.java
+++ Hive/Hive_HW3/src/test/java/hive/hw3/UserAgentUdf2Test.java
@@ -1,10 +1,10 @@
 package hive.hw3;
 
+import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StandardStructObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StructField;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaStringObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.junit.Test;
 
@@ -14,24 +14,36 @@ import static org.junit.Assert.assertThat;
 public class UserAgentUdf2Test {
 
     @Test
-    public void evaluate() throws Exception {
-        UserAgentUdf2 agent2 = new UserAgentUdf2();
-        JavaStringObjectInspector stringOI = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
-//        StandardListObjectInspector listOI = ObjectInspectorFactory.getStandardListObjectInspector(stringOI);
-        StandardStructObjectInspector resultOI = agent2.initialize(new ObjectInspector[]{stringOI});
+    public void bot() throws HiveException {
+        testUserAgent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                "BOT", "UNKNOWN", "");
+    }
 
-        String userAgent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+    @Test
+    public void firefox() throws HiveException {
+        testUserAgent("Mozilla/5.0 (Windows NT 5.1; rv:24.0) Gecko/20100101 Firefox/24.0",
+                "FIREFOX24", "WINDOWS_XP", "24.0");
+    }
+
+    private void testUserAgent(String userAgent, String expBrowser, String expOs, String expBrowserVersion) throws HiveException {
+        UserAgentUdf2 agent2 = new UserAgentUdf2();
+        StandardStructObjectInspector resultOI = agent2.initialize(
+                new ObjectInspector[]{PrimitiveObjectInspectorFactory.javaStringObjectInspector});
+
         GenericUDF.DeferredJavaObject object = new GenericUDF.DeferredJavaObject(userAgent);
-//        new GenericUDF.DeferredObject[]{new GenericUDF.DeferredJavaObject(list), object};
+
         Object result = agent2.evaluate(new GenericUDF.DeferredObject[]{object});
+
         StructField browserField = resultOI.getStructFieldRef(UserAgentUdf2.browserFieldName);
         StructField browserVersionField = resultOI.getStructFieldRef(UserAgentUdf2.browserVersionFieldName);
         StructField osField = resultOI.getStructFieldRef(UserAgentUdf2.osFieldName);
+
         String browserFieldData = (String) resultOI.getStructFieldData(result, browserField);
         String browserVersionFieldData = (String) resultOI.getStructFieldData(result, browserVersionField);
         String osFieldData = (String) resultOI.getStructFieldData(result, osField);
-        assertThat(browserFieldData, equalTo("BOT"));
-        assertThat(browserVersionFieldData, equalTo(""));
-        assertThat(osFieldData, equalTo("UNKNOWN"));
+
+        assertThat(browserFieldData, equalTo(expBrowser));
+        assertThat(browserVersionFieldData, equalTo(expBrowserVersion));
+        assertThat(osFieldData, equalTo(expOs));
     }
 }
