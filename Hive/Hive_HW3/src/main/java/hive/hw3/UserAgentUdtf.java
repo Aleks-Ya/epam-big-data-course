64059f0357ba96d29890d57abc9d4f08b73503e5
Aleksei Iablokov <aleksei_iablokov@epam.com>
2017-04-11 13:09:52 +0300

    cleanup

--- Hive/Hive_HW3/src/main/java/hive/hw3/UserAgentUdtf.java
+++ Hive/Hive_HW3/src/main/java/hive/hw3/UserAgentUdtf.java
@@ -8,40 +8,30 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.StandardStructObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory.javaStringObjectInspector;
 
-public class UserAgentUdf4 extends GenericUDTF {
-    private static final Logger LOG = LoggerFactory.getLogger(UserAgentUdf4.class);
-    static final String browserFieldName = "browser";
-    static final String deviceFieldName = "device";
-    static final String osFieldName = "os";
+@SuppressWarnings("unused")
+public class UserAgentUdtf extends GenericUDTF {
 
     @Override
     public StandardStructObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
-        LOG.error("Initialize: " + getClass());
         return createObjectInspector();
     }
 
     @Override
     public void process(Object[] objects) throws HiveException {
-        LOG.error("Evaluate: " + Arrays.deepToString(objects));
         if (objects == null) {
-            LOG.error("Objects is null. Return.");
             return;
         }
         if (objects.length != 1) {
             throw new IllegalArgumentException("Expected 1 argument, but found " + objects.length);
         }
         String userAgent = PrimitiveObjectInspectorFactory.javaStringObjectInspector.getPrimitiveJavaObject(objects[0]);
-        LOG.error("User agent: " + userAgent);
 
         UserAgent agent = UserAgent.parseUserAgentString(userAgent);
         String browserData = agent.getBrowser().toString();
@@ -53,14 +43,13 @@ public class UserAgentUdf4 extends GenericUDTF {
 
     @Override
     public void close() throws HiveException {
-
     }
 
     private StandardStructObjectInspector createObjectInspector() {
         List<String> fieldNames = new ArrayList<>();
-        fieldNames.add(browserFieldName);
-        fieldNames.add(deviceFieldName);
-        fieldNames.add(osFieldName);
+        fieldNames.add("browser");
+        fieldNames.add("device");
+        fieldNames.add("os");
 
         List<ObjectInspector> fieldOIs = Collections.nCopies(3, javaStringObjectInspector);
         return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
